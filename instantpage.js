1ced51f28012de32b7f22c98616065363123a6e3
Alexandre Dieulot <adieulot@gmail.com>
2020-02-17 12:15:13 +0100

    Update copyright year

--- instantpage.js
+++ instantpage.js
@@ -1,4 +1,4 @@
-/*! instant.page v3.0.0 - (C) 2019 Alexandre Dieulot - https://instant.page/license */
+/*! instant.page v3.0.0 - (C) 2019-2020 Alexandre Dieulot - https://instant.page/license */
 
 let mouseoverTimer
 let lastTouchTimestamp
