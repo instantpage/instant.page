12a54d526b5fbcf2d82f83726e426e3c11537ea7
ivansimplistic <38053326+ivansimplistic@users.noreply.github.com>
2023-11-30 14:06:30 -0300

    changed previous fix

--- instantpage.js
+++ instantpage.js
@@ -198,11 +198,12 @@ function mouseoverListener(event) {
 
   anchorElement.addEventListener('mouseout', mouseoutListener, {passive: true})
 
-  clearTimeout(_mouseoverTimer); //sometimes mouseover triggers a few times before mouseout, setting multiple timeouts but then mouseout only clears the last timeout.
-  _mouseoverTimer = setTimeout(() => {
-    preload(anchorElement.href, 'high')
-    _mouseoverTimer = undefined
-  }, _delayOnHover)
+  if (!_mouseoverTimer) { //sometimes mouseover triggers a few times before mouseout, setting multiple timeouts but then mouseout only clears the last timeout.
+    _mouseoverTimer = setTimeout(() => {
+      preload(anchorElement.href, 'high')
+      _mouseoverTimer = undefined
+    }, _delayOnHover)
+  }
 }
 
 function mousedownListener(event) {
